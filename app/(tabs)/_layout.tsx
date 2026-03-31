220b7198e00069fe12c6f3e63858f394bd79b14d
Marcos-devclub2024 <monteiromarcosk@gmail.com>
2026-03-30 23:01:37 -0300

    Identidade Visual IPAA e Logo da Arca

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,45 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#b21f1f", // Cor vermelha quando selecionado
-        headerShown: false,
+        tabBarActiveTintColor: "#FFD700",
+        tabBarInactiveTintColor: "#a0a0a0",
+        tabBarStyle: {
+          backgroundColor: "#002F6C",
+          height: 65,
+          paddingBottom: 8,
+          borderTopWidth: 2,
+          borderTopColor: "#FFD700",
+        },
+        headerStyle: {
+          backgroundColor: "#002F6C",
+          height: 140,
+        },
+        headerTitleAlign: "center",
+        headerTitle: () => (
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 15,
+            }}
+          >
+            <Image
+              // NOME CORRIGIDO ABAIXO:
+              source={require("../../assets/images/logo_arte.png")}
+              style={{
+                width: 180,
+                height: 110,
+              }}
+              resizeMode="contain"
+            />
+          </View>
+        ),
       }}
     >
       <Tabs.Screen
@@ -14,7 +47,16 @@ export default function TabLayout() {
         options={{
           title: "Início",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="home" size={24} color={color} />
+            <Ionicons name="home" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mural"
+        options={{
+          title: "Mural",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="megaphone" size={26} color={color} />
           ),
         }}
       />
@@ -23,7 +65,7 @@ export default function TabLayout() {
         options={{
           title: "Membros",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="people" size={24} color={color} />
+            <Ionicons name="people" size={26} color={color} />
           ),
         }}
       />
